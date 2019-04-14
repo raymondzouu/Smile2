f472fbeae19d19dab0fd914b3b45603e5f3a9aae
raymondzouu <31597464+raymondzouu@users.noreply.github.com>
2019-04-14 07:21:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 console.log("It worked!");
 
     //Pop-up stuff
-    document.body.innerHTML += '<dialog id="dialog" style="width: 320px; height: 320px;"><center><font size = "+10">Smile!</font></center><center><br><button>Close</button></center><video id="video" width="320" height="240" autoplay="true"></video></dialog>';
+    document.body.innerHTML += '<dialog id="dialog" style="width: 320px; height: 320px;"><center><font size = "+10">Smile!</font></center><video id="video" width="320" height="240" autoplay="true"></video><center><br><button>Close</button></center></dialog>';
     var dialog = document.querySelector("dialog")
     dialog.querySelector("button").addEventListener("click", function() {
         dialog.close()
@@ -38,82 +38,82 @@ console.log("It worked!");
         var r = new FileReader();
         r.onload = function(){alert(r.result);};
         console.log(image);
-
-        src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js';
-        processImage();
-
-
-
-        function processImage() {
-            // Replace <Subscription Key> with your valid subscription key.
-            var subscriptionKey = "24c30a3980384e43bd3cd274a360dcc8";
-
-            // NOTE: You must use the same region in your REST call as you used to
-            // obtain your subscription keys. For example, if you obtained your
-            // subscription keys from westus, replace "westcentralus" in the URL
-            // below with "westus".
-            //
-            // Free trial subscription keys are generated in the "westus" region.
-            // If you use a free trial subscription key, you shouldn't need to change
-            // this region.
-            var uriBase =
-                "https://westus.api.cognitive.microsoft.com/face/v1.0/detect";
-
-            // Request parameters.
-            var params = {
-                "returnFaceAttributes": "smile"
-            };
-
-            var imageURL = ("https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg").value;
-
-            console.log("Requested!");
-
-            // Display the image.
-            // var sourceImageUrl = document.getElementById("inputImage").value;
-            // document.querySelector("#sourceImage").src = sourceImageUrl;
-
-            // Perform the REST API call.
-            $.ajax({
-                url: uriBase + "?" + $.param(params),
-
-                // Request headers.
-                beforeSend: function(xhrObj){
-                    xhrObj.setRequestHeader("Content-Type","application/json");
-                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
-                },
-
-                type: "POST",
-
-                // Request body.
-                data: '{"url": ' + '"' + imageURL + '"}',
-            })
-
-
-            .done(function(data) {
-                // Show formatted JSON on webpage.
-                console.log("Done!");
-                console.log("Smile: " + data.smile);
-                $("#responseTextArea").val(JSON.stringify(data, null, 2));
-                if(data.smile > .75){
-                    dialog.close()
-                }
-            })
+        alert('You look great today!');
+//         src = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js';
+//         processImage();
 
 
 
+//         function processImage() {
+//             // Replace <Subscription Key> with your valid subscription key.
+//             var subscriptionKey = "24c30a3980384e43bd3cd274a360dcc8";
+
+//             // NOTE: You must use the same region in your REST call as you used to
+//             // obtain your subscription keys. For example, if you obtained your
+//             // subscription keys from westus, replace "westcentralus" in the URL
+//             // below with "westus".
+//             //
+//             // Free trial subscription keys are generated in the "westus" region.
+//             // If you use a free trial subscription key, you shouldn't need to change
+//             // this region.
+//             var uriBase =
+//                 "https://westus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+//             // Request parameters.
+//             var params = {
+//                 "returnFaceAttributes": "smile"
+//             };
+
+//             var imageURL = ("https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg").value;
+
+//             console.log("Requested!");
+
+//             // Display the image.
+//             // var sourceImageUrl = document.getElementById("inputImage").value;
+//             // document.querySelector("#sourceImage").src = sourceImageUrl;
+
+//             // Perform the REST API call.
+//             $.ajax({
+//                 url: uriBase + "?" + $.param(params),
+
+//                 // Request headers.
+//                 beforeSend: function(xhrObj){
+//                     xhrObj.setRequestHeader("Content-Type","application/json");
+//                     xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+//                 },
+
+//                 type: "POST",
+
+//                 // Request body.
+//                 data: '{"url": ' + '"' + imageURL + '"}',
+//             })
 
 
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                // Display error message.
-                var errorString = (errorThrown === "") ?
-                    "Error.jjjjj " : errorThrown + " (" + jqXHR.status + "): ";
-                errorString += (jqXHR.responseText === "") ?
-                    "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
-                        jQuery.parseJSON(jqXHR.responseText).message :
-                            jQuery.parseJSON(jqXHR.responseText).error.message;
-                alert(errorString);
-            });
-        };
+//             .done(function(data) {
+//                 // Show formatted JSON on webpage.
+//                 console.log("Done!");
+//                 console.log("Smile: " + data.smile);
+//                 $("#responseTextArea").val(JSON.stringify(data, null, 2));
+//                 if(data.smile > .75){
+//                     dialog.close()
+//                 }
+//             })
+
+
+
+
+
+//             .fail(function(jqXHR, textStatus, errorThrown) {
+//                 // Display error message.
+//                 var errorString = (errorThrown === "") ?
+//                     "Error.jjjjj " : errorThrown + " (" + jqXHR.status + "): ";
+//                 errorString += (jqXHR.responseText === "") ?
+//                     "" : (jQuery.parseJSON(jqXHR.responseText).message) ?
+//                         jQuery.parseJSON(jqXHR.responseText).message :
+//                             jQuery.parseJSON(jqXHR.responseText).error.message;
+//                 alert(errorString);
+//             });
+//         };
         //const detections = await detectAllFaces(image);
         //console.log(detections)
 
